b569a2ab61311556575d5c559c42f89179f7a810
kiorky <kiorky@cryptelium.net>
2011-03-29 16:36:40 +0200

    up

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -8,7 +8,7 @@ def h(options, buildout):
     bd=buildout['buildout']["directory"]
     f =  open(os.path.join(buildout['buildout']['directory'], 'cmake.cmake'), 'w')
     f.write(buildout['init']['init']+"\n")
-    os.environ['LDFLAGS'] = os.environ.get('LDFLAGS', '') + '-lz -lcurl -lexpat'
+    os.environ['LDFLAGS'] = os.environ.get('LDFLAGS', '') + ' -lz -lcurl -lexpat'
     p(options, buildout)
 
 
